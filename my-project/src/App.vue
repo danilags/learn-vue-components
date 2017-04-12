ee2d7b5c545cfa936ab3c3827e89f42dc426372c
hacktiv8 <aguspermadi94@gmail.com>
2017-04-12 22:29:45 +0700

    done

---
+++ my-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <manage-products></manage-products>
+  </div>
+
+</template>
+
+<script>
+import ProductList from './components/ProductList';
+import SaveProductForm from './components/SaveProductForm';
+import ManageProducts from './components/ManageProducts';
+
+export default {
+
+  components: {
+    ProductList,
+    SaveProductForm,
+    ManageProducts
+  }
+}
+
+</script>
---
+++ my-project/src/components/ProductList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1 class="header">Game Card</h1>
+    <table class="table table-hover product-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="product in products" track-by="id">
+
+          <td>{{product.name}}</td>
+          <td>{{product.description}}</td>
+          <td>{{product.price}}:-</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['products']
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .header {
+    font-size: 4rem;
+  }
+</style>
